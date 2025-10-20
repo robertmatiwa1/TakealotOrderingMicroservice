7766fff03f3ff051759cfbd2662a2cb133c08e76
Robert Matiwa <robertmatiwa3@gmail.com>
2025-10-20 20:18:13 +0200

    Refined API structure, added Swagger UI, observability, and documentation updates

--- Ordering.Application/obj/Debug/net8.0/Ordering.Application.AssemblyInfo.cs
+++ Ordering.Application/obj/Debug/net8.0/Ordering.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ordering.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb38d591e8f591d2a4fadb345a9c70daf7b608df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87863cd524400d23902e83ea5ed6de182fba7549")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ordering.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ordering.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
