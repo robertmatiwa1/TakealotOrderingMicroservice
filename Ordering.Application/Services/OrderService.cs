87863cd524400d23902e83ea5ed6de182fba7549
Robert Matiwa <robertmatiwa3@gmail.com>
2025-10-20 02:11:56 +0200

    stabilised ordering microservice build - Swagger, Serilog, and OpenTelemetry fixed

--- Ordering.Application/Services/OrderService.cs
+++ Ordering.Application/Services/OrderService.cs
@@ -1,8 +1,13 @@
-using Ordering.Application.Interfaces;
-using Ordering.Domain;
-using Ordering.Infrastructure.Outbox;
-using Ordering.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Ordering.Domain;
+using Ordering.Application.Interfaces;
+using Ordering.Infrastructure.Persistence;
+using Ordering.Infrastructure.Outbox;
 
 namespace Ordering.Application.Services
 {
--- Ordering.Application/obj/Debug/net8.0/Ordering.Application.AssemblyInfo.cs
+++ Ordering.Application/obj/Debug/net8.0/Ordering.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ordering.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a919d6112f6cbf7adb66255a58b4162f0fe542a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb38d591e8f591d2a4fadb345a9c70daf7b608df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ordering.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ordering.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
